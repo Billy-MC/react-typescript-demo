46fad640a47161bf53ee2eb948469d99fea8b9c9
Billy Chui <billy2022002@gmail.com>
2021-12-10 15:57:53 +1100

    wrap HTML elements

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ import './App.css';
 // import { Private } from './components/auth/Private';
 // import { Profile } from './components/auth/Profile';
 // import { List } from './components/generics/List';
-import { RandomNumber } from './components/restriction/RandomNumber';
+// import { RandomNumber } from './components/restriction/RandomNumber';
+// import { Toast } from './components/templateliterals/Toast';
+import { CustomButton } from './components/html/Button';
 
 function App() {
 	// const personName = {
@@ -89,7 +91,11 @@ function App() {
 				]}
 				onClick={item => console.log(item)}
 			/> */}
-			<RandomNumber value={10} isPositive />
+			{/* <RandomNumber value={10} isPositive /> */}
+			{/* <Toast position='left-top' /> */}
+			<CustomButton variant='primary' onClick={() => console.log('Clicked')}>
+				Primary Button
+			</CustomButton>
 		</div>
 	);
 }
