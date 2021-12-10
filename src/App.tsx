e154e02f2bbef843fdef1649496e7ed189ec7671
Billy Chui <billy2022002@gmail.com>
2021-12-10 16:42:52 +1100

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,8 @@ import './App.css';
 // import { List } from './components/generics/List';
 // import { RandomNumber } from './components/restriction/RandomNumber';
 // import { Toast } from './components/templateliterals/Toast';
-import { CustomButton } from './components/html/Button';
+// import { CustomButton } from './components/html/Button';
+import { Text } from './components/polymorphic/Text';
 
 function App() {
 	// const personName = {
@@ -93,11 +94,20 @@ function App() {
 			/> */}
 			{/* <RandomNumber value={10} isPositive /> */}
 			{/* <Toast position='left-top' /> */}
-			<CustomButton variant='primary' onClick={() => console.log('Clicked')}>
-				{/* <div>Primary Button</div> */}
-				{/* 因為restricted the type of children just String */}
-				Primary Button
-			</CustomButton>
+			{/* <CustomButton variant='primary' onClick={() => console.log('Clicked')}> */}
+			{/* <div>Primary Button</div> */}
+			{/* 因為restricted the type of children just String */}
+			{/* Primary Button
+			</CustomButton> */}
+			<Text as='h1' size='lg'>
+				Heading
+			</Text>
+			<Text as='p' size='md'>
+				Paragraph
+			</Text>
+			<Text as='label' size='sm' color='secondary'>
+				Label
+			</Text>
 		</div>
 	);
 }
